916ffdc1fb69d4c8207e9ce80bd3838236a25146
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-30 17:48:18 +0100

    add docstrings

--- base/accounts/models.py
+++ base/accounts/models.py
@@ -5,9 +5,16 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
+    '''
+    Overrides Django's default UserManager.
+    '''
     use_in_migrations: True
 
     def _create_user(self, email, password=None, **extra_fields):
+        '''
+        Saves a user to the database, wether it is a simple user
+        or a super user.
+        '''
         if not email:
             raise ValueError('Users must have an email address')
         email = self.normalize_email(email)
@@ -19,11 +26,17 @@ class UserManager(BaseUserManager):
 
 
     def create_user(self, email, password=None, **extra_fields):
+        '''
+        Overrides Django's default create_user method of the User model.
+        '''
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
+        '''
+        Overrides Django's default create_superuser method of the User model.
+        '''
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -36,6 +49,13 @@ class UserManager(BaseUserManager):
 
 
 class CustomUser(AbstractUser):
+    '''
+    CustomUser model that substitues Django's built-in
+    User model. It uses the email as the identification
+    token instead of the username.AUTH_USER_MODEL in settings.py
+    must point to the CustomUser model.
+    https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+    '''
     username = None
     email = models.EmailField(unique=True)
 
