53c9732cd7bdf6f360a0e1bcb6589440e3f88f2f
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-30 16:24:44 +0100

    add password1 and password2 fields to add_fieldsets attribute inside CustomUserAdmin.without these fields it was not possible to add a user inside the admin area

--- base/accounts/admin.py
+++ base/accounts/admin.py
@@ -20,7 +20,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password')
+            'fields': ('email', 'password1', 'password2')
         }),
     )
     search_fields = ('email',)
