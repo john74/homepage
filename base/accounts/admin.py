ea844d3347f58128b761190b820a6bde83ea500a
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-30 14:48:15 +0100

    add fieldsets and add_fieldsets attributes to CustomUserAdmin model

--- base/accounts/admin.py
+++ base/accounts/admin.py
@@ -3,11 +3,30 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
+
 class CustomUserAdmin(UserAdmin):
+    # https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#custom-users-and-django-contrib-admin
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ["email", "password"]
+
+    # fields to be used in editing users
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+    )
+
+    # fields to be used when creating users
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password')
+        }),
+    )
+    search_fields = ('email',)
     ordering = ('email',)
+    filter_horizontal = ()
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.unregister(Group)
