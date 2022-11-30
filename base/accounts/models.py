747c996359204b16a23c5e2cbd30db260322cdd5
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-30 14:49:05 +0100

    set password parameter default value to None

--- base/accounts/models.py
+++ base/accounts/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class UserManager(BaseUserManager):
     use_in_migrations: True
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
         email = self.normalize_email(email)
@@ -16,12 +16,14 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+
+
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
