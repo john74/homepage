2b7d4bf97fe6616a935f712c65410926505e258e
JohnEf <eustathiou.ioan@protonmail.com>
2022-12-11 11:34:34 +0100

    add form_method field to SearchEngine model

--- base/home/models.py
+++ base/home/models.py
@@ -5,6 +5,7 @@ class SearchEngine(models.Model):
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=700)
     form_action = models.CharField(max_length=200)
+    form_method = models.CharField(max_length=200)
     name_attribute = models.CharField(max_length=100)
     default = models.BooleanField(default=False)
 
