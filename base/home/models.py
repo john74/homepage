e3c11fa5b1fa07f6bcf5321b9a50f1a02f7ae939
JohnEf <eustathiou.ioan@protonmail.com>
2022-12-11 10:50:53 +0100

    create SearchEngine model

--- base/home/models.py
+++ base/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class SearchEngine(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=700)
+    form_action = models.CharField(max_length=200)
+    name_attribute = models.CharField(max_length=100)
+    default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
