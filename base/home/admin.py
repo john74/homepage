8567fe8bf9fc369ac8619399914eb67b59c91b2d
JohnEf <eustathiou.ioan@protonmail.com>
2022-12-12 19:16:29 +0100

    register ApiKey model

--- base/home/admin.py
+++ base/home/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import SearchEngine, BookmarkCategory, Bookmark
+from .models import SearchEngine, BookmarkCategory, Bookmark, ApiKey
 
 
 admin.site.register(SearchEngine)
 admin.site.register(BookmarkCategory)
 admin.site.register(Bookmark)
+admin.site.register(ApiKey)
