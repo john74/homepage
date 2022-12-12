373fbeb894aef1003c0e36dfb2925591f3c4c0cc
JohnEf <eustathiou.ioan@protonmail.com>
2022-12-12 19:10:47 +0100

    create ApiKeys model

--- base/home/models.py
+++ base/home/models.py
@@ -38,3 +38,9 @@ class Bookmark(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ApiKeys(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    key = models.CharField(max_length=300)
