f2b4afbb5c028229099d866a51668d58886fce11
JohnEf <eustathiou.ioan@protonmail.com>
2022-12-18 16:28:45 +0100

    create/register Interface, Profile, Email, ApiKey models

--- base/settings/admin.py
+++ base/settings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Interface, Profile, Email, ApiKey
 
-# Register your models here.
+
+admin.site.register(Interface)
+admin.site.register(Profile)
+admin.site.register(Email)
+admin.site.register(ApiKey)
--- base/settings/models.py
+++ base/settings/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class Interface(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    theme_light = models.BooleanField(default=False)
+    sidebar_right = models.BooleanField(default=False)
+    primary_color = models.CharField(max_length=10)
+    secondary_color = models.CharField(max_length=10)
+    text_color = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name_plural = 'Interface'
+
+    def __str__(self):
+        return 'Interface'
+
+class Profile(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Profile'
+
+    def __str__(self):
+        return self.username
+
+
+class Email(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+    password = models.CharField(max_length=50, null=True, blank=True)
+    indication_color = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name_plural = 'Email'
+
+    def __str__(self):
+        return self.name
+
+class ApiKey(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    key = models.CharField(max_length=500)
+
+    class Meta:
+        verbose_name_plural = 'Api Keys'
+
+    def __str__(self):
+        return self.name
