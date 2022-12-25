c2a81e94cd133097cb6bb600efe7cd6ff3fabbd3
JohnEf <eustathiou.ioan@protonmail.com>
2022-12-25 11:34:44 +0100

    remove country and city fields

--- base/accounts/models.py
+++ base/accounts/models.py
@@ -58,8 +58,6 @@ class CustomUser(AbstractUser):
     '''
     username = None
     email = models.EmailField(unique=True)
-    country = models.CharField(max_length=100, null=True, blank=True)
-    city = models.CharField(max_length=100, null=True, blank=True)
 
     objects = UserManager()
 
