323b3dcd1865743c0e258cc5923cb782a4f64c90
JohnEf <eustathiou.ioan@protonmail.com>
2022-12-25 12:03:17 +0100

    register EmailService

--- base/settings/admin.py
+++ base/settings/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Interface, Profile, Email, ApiKey
+from .models import Interface, Profile, EmailService, Email, ApiKey
 
 
 admin.site.register(Interface)
 admin.site.register(Profile)
+admin.site.register(EmailService)
 admin.site.register(Email)
 admin.site.register(ApiKey)
