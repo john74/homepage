00c2dc5c0696d4ffc1aca8d9350745de598e398d
JohnEf <eustathiou.ioan@protonmail.com>
2022-12-25 18:39:06 +0100

    finish saving email form

--- base/settings/views.py
+++ base/settings/views.py
@@ -6,31 +6,39 @@ from .forms import ProfileForm
 from .forms import InterfaceForm
 from .forms import EmailForm
 from .forms import ApiKeyForm
-from .utils import get_or_create_email_services
+from .utils import create_email_services
 from .utils import get_or_create_profile
 from .utils import get_profile_form_data
 from .utils import save_profile_form
 from .utils import get_email_form_data
+from .utils import get_email_accounts
+from .utils import get_zipped_email_form_data
+from .utils import get_zipped_email_db_data
+from .utils import get_changed_email_data
+from .utils import save_email_form
 
 
 def settings(request):
-    user = CustomUser.objects.get(email=request.user.email)
-    emails = get_or_create_email_services(user)
-    profile = get_or_create_profile(user)
+    user_id = request.user.id
+    user = CustomUser.objects.get(id=user_id)
+    create_email_services(user)
+
+    email_services = get_email_accounts(user)
 
     if request.method == 'POST':
         data = request.POST
 
-        profile_form_data = get_profile_form_data(data, profile)
-        if profile_form_data:
-            save_profile_form(profile_form_data)
+        email_form_data = get_email_form_data(data)
+        zipped_email_form_data = get_zipped_email_form_data(email_form_data)
+        zipped_email_db_data = get_zipped_email_db_data(email_services)
+        changed_email_data = get_changed_email_data(zipped_email_form_data, zipped_email_db_data)
 
-        email_form_data = get_email_form_data(data, emails)
+        if changed_email_data:
+            save_email_form(changed_email_data)
 
         return redirect('settings')
 
     context = {
-        'profile': profile,
-        'emails': emails
+        'email_services': email_services
     }
     return render(request, 'settings.html', context)
