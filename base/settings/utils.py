dc36d87b6964970201b80bca15cf8732b24cb34c
JohnEf <eustathiou.ioan@protonmail.com>
2022-12-26 21:03:52 +0100

    refactor data grouping and form saving

--- base/settings/utils.py
+++ base/settings/utils.py
@@ -33,173 +33,72 @@ def get_email_accounts(user):
     return accounts
 
 
-def get_email_form_data(form_data):
-    email_model_field_names = [field.name for field in Email._meta.get_fields()]
-    email_form_data = {}
-    for field_name in email_model_field_names:
-        form_field_values = form_data.getlist(f'email-{field_name}')
-        email_form_data[field_name] = form_field_values
-    return email_form_data
-
-
-def get_zipped_email_form_data(form_data):
-    return zip(
-        form_data['id'],
-        form_data['service'],
-        form_data['email'],
-        form_data['password'],
-        form_data['category'],
-        form_data['color']
-    )
-
-
-def get_zipped_email_db_data(services):
-    data = []
-    for accounts in services.values():
-        for account in accounts:
-            data.append(tuple([
-                str(account.id),
-                str(account.service.id),
-                account.email,
-                account.password,
-                account.category,
-                account.color
-            ]))
-    return data
-
-
-def get_changed_email_data(form_data, db_data):
-    changed_data = []
-    for data in form_data:
-        if data not in db_data:
-            changed_data.append(data)
-    return changed_data
-
-
-def save_email_form(email_data):
-    for data in email_data:
-        email_id = data[0]
-
-        try:
-            email_instance = Email.objects.get(id=email_id)
-        except Email.DoesNotExist:
-            email_instance = None
-
-        form = EmailForm(
-            {
-                'email': data[2],
-                'password': data[3],
-                'category': data[4],
-                'color': data[5]
-            },
-            instance=email_instance
-        )
-
-        service_id = data[1]
-        try:
-            service = EmailService.objects.get(id=service_id)
-        except EmailService.DoesNotExist:
-            return
-
-        if form.is_valid():
-            email_form = form.save(commit=False)
-            email_form.service = service
-            email_form.save()
-
-
-def get_or_create_profile(user):
-    try:
-        return Profile.objects.get(user=user.id)
-    except Profile.DoesNotExist:
-        return Profile.objects.create(user=user)
-
-
-def get_form_data(model, form_data):
-    model_field_names = [field.name for field in model._meta.get_fields()]
-    model_form_data = {}
-    field_prefix = model._meta.verbose_name.lower()
-    for field_name in model_field_names:
-        if field_name in ['user']:
-            continue
-        form_field_values = form_data.getlist(f'{field_prefix}-{field_name}')
-        model_form_data[field_name] = form_field_values[0]
-    return model_form_data
-
-
-def get_profile_db_data(profile):
-    return {
-        'id': str(profile.id),
-        'username': profile.username,
-        'country': profile.country,
-        'city': profile.city
-    }
-
-
-def get_changed_profile_data(form_data, db_data):
-    if form_data != db_data:
-        return form_data
-    return
-
-
-def save_profile_form(data):
-    profile_id = data['id']
-    try:
-        profile = Profile.objects.get(id=profile_id)
-    except Profile.DoesNotExist:
-        return
-    form = ProfileForm(data, instance=profile)
-    if form.is_valid():
-        profile_form = form.save(commit=False)
-        profile_form.user = profile.user
-        profile_form.save()
-    return
-
-
-def group_data(data):
-    d = []
+def group_test_data(data):
+    grouped_data = []
     first_key = next(iter(data))
-    max_loops = len(data[first_key])
-    for loop in range(max_loops):
+    loops = len(data[first_key])
+    for index in range(loops):
         temp = {}
-        for key, value in data.items():
-            temp[key] = value[loop]
-        d.append(temp)
-    return d
+        for key, value in  data.items():
+            temp[key] = value[index]
+        grouped_data.append(temp)
+    return grouped_data
 
 
-def get_api_form_data(model, form_data):
-    model_field_names = [field.name for field in model._meta.get_fields()]
-    model_form_data = {}
-    field_prefix = model._meta.verbose_name.lower()
-    for field_name in model_field_names:
-        form_field_values = form_data.getlist(f'{field_prefix}-{field_name}')
-        if form_field_values:
-            model_form_data[field_name] = form_field_values
-    return group_data(model_form_data)
+def group_form_data(form_data, prefix):
+    data = {}
+    for field_name in form_data.keys():
+        if field_name.startswith(prefix):
+            values = form_data.getlist(field_name)
+            db_field_name = field_name.replace(f'{prefix}-', '')
+            data[db_field_name] = values
+    # print('GROUPED FORM DATA -> ', group_test_data(data))
+    return group_test_data(data)
+
+def sanitize_object(value, form_data):
+    form_fields = form_data[0].keys()
+    fields_to_remove = []
+    for field in value.keys():
+        if field not in form_fields:
+            fields_to_remove.append(field)
+        if isinstance(value[field], int):
+            value[field] = str(value[field])
+    for field in fields_to_remove:
+        del value[field]
+    return value
 
 
-def get_zipped_api_db_data(form_data):
+def group_db_data(db_data, form_data):
     data = []
-    for value in form_data.values():
-        del value['user_id']
-        value['id'] = str(value['id'])
-        data.append(value)
+    for value in db_data.values():
+        obj = sanitize_object(value, form_data)
+        data.append(obj)
+    # print('TEST DB DATA -> ', data)
     return data
 
 
-def get_changed_api_data(form_data, db_data):
+def get_changed_data(form_data, db_data):
     changed_data = []
     for data in form_data:
         if data not in db_data:
             changed_data.append(data)
     return changed_data
 
-def save_api_form(api_data):
-    for data in api_data:
+
+def save_form(model, form, form_data):
+    for data in form_data:
         try:
-            instance = ApiKey.objects.get(id=data['id'])
-        except ApiKey.DoesNotExist:
-            continue
-        form = ApiKeyForm(data, instance=instance)
-        if form.is_valid():
-            form.save()
+            instance = model.objects.get(id=data['id'])
+        except model.DoesNotExist:
+            instance = None
+
+        model_form = form(data, instance=instance)
+        if model_form.is_valid():
+            new_form = model_form.save(commit=False)
+            if model.__name__.lower() == 'email':
+                try:
+                    service = EmailService.objects.get(id=data['service_id'])
+                except EmailService.DoesNotExist:
+                    continue
+                new_form.service = service
+            new_form.save()
--- base/settings/views.py
+++ base/settings/views.py
@@ -1,28 +1,20 @@
 from django.contrib.auth.hashers import make_password
 from django.shortcuts import render, redirect
 from accounts.models import CustomUser
-from .models import Profile, Email, ApiKey
+from .models import Profile
+from .models import Email
+from .models import ApiKey
 from .forms import ProfileForm
 from .forms import InterfaceForm
 from .forms import EmailForm
 from .forms import ApiKeyForm
 from .utils import create_email_services
-from .utils import get_or_create_profile
-from .utils import save_profile_form
-from .utils import get_email_form_data
 from .utils import get_email_accounts
-from .utils import get_zipped_email_form_data
-from .utils import get_zipped_email_db_data
-from .utils import get_changed_email_data
-from .utils import save_email_form
-from .utils import get_form_data
-from .utils import get_profile_db_data
-from .utils import get_changed_profile_data
 from .utils import create_api_services
-from .utils import get_api_form_data
-from .utils import get_zipped_api_db_data
-from .utils import get_changed_api_data
-from .utils import save_api_form
+from .utils import group_form_data
+from .utils import group_db_data
+from .utils import get_changed_data
+from .utils import save_form
 
 
 def settings(request):
@@ -31,39 +23,39 @@ def settings(request):
     create_email_services(user)
     create_api_services(user)
 
-    profile = Profile.objects.get(user=user_id)
+    profiles = Profile.objects.filter(user=user_id)
     email_services = get_email_accounts(user)
     apis = ApiKey.objects.filter(user=user.id)
 
     if request.method == 'POST':
         data = request.POST
+        # print('POST DATA -> ', data)
 
-        profile_form_data = get_form_data(Profile, data)
-        profile_db_data = get_profile_db_data(profile)
-        changed_profile_data = get_changed_profile_data(profile_form_data, profile_db_data)
+        grouped_profile_form_data = group_form_data(data, 'profile')
+        grouped_profile_db_data = group_db_data(profiles, grouped_profile_form_data)
+        changed_profile_data = get_changed_data(grouped_profile_form_data, grouped_profile_db_data)
         if changed_profile_data:
-            save_profile_form(changed_profile_data)
+            save_form(Profile, ProfileForm, changed_profile_data)
 
-        email_form_data = get_email_form_data(data)
-        zipped_email_form_data = get_zipped_email_form_data(email_form_data)
-        zipped_email_db_data = get_zipped_email_db_data(email_services)
-        changed_email_data = get_changed_email_data(zipped_email_form_data, zipped_email_db_data)
-
+        emails = Email.objects.filter(service__user=user)
+        grouped_email_form_data = group_form_data(data, 'email')
+        grouped_email_db_data = group_db_data(emails, grouped_email_form_data)
+        changed_email_data = get_changed_data(grouped_email_form_data, grouped_email_db_data)
         if changed_email_data:
-            save_email_form(changed_email_data)
+            save_form(Email, EmailForm, changed_email_data)
 
-        api_form_data = get_api_form_data(ApiKey, data)
-        api_db_data = get_zipped_api_db_data(apis)
-        changed_api_data = get_changed_api_data(api_form_data, api_db_data)
+        grouped_api_form_data = group_form_data(data, 'api')
+        grouped_api_db_data = group_db_data(apis, grouped_api_form_data)
+        changed_api_data = get_changed_data(grouped_api_form_data, grouped_api_db_data)
         if changed_api_data:
-            save_api_form(changed_api_data)
+            save_form(ApiKey, ApiKeyForm, changed_api_data)
 
 
 
         return redirect('settings')
 
     context = {
-        'profile': profile,
+        'profiles': profiles,
         'email_services': email_services,
         'apis': apis
     }
